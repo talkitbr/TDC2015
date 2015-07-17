dd689f35b53a353ae426bb97dfd242f570cf6ec2
João Ronaldo Del-Ducca Cunha <joao.cunha@eldorado.org.br>
2015-07-17 14:59:05 -0300

    Added Background Task

---
+++ TDC2015/TDC2015.BackgroundTask/TileUpdaterTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace TDC2015.BackgroundTask
+{
+    public sealed class TileUpdaterTask : IBackgroundTask
+    {
+        private static string w10TileXml = @"<tile>
+                                <visual>
+                                 <binding template=""TileSmall"">
+                                  <text>TDC</text>
+                                  <text>UW</text>
+                                 </binding>
+                                 <binding template=""TileMedium"">
+                                  <image placement=""peek"" src=""Assets\Tile150x150.png"" />
+                                  <image src=""Assets\Tile_w10_150x150.png"" />                                  
+                                 </binding>
+                                 <binding template=""TileWide"">                                  
+                                  <group>
+                                   <subgroup hint-weight=""33"">
+                                    <image placement=""inline"" src=""Assets\TDC_w10_310X150.png"" />
+                                   </subgroup>
+                                   <subgroup>
+                                    <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"">
+                                      Trilha Universal Windows
+                                    </text>
+                                    <text hint-style=""captionsubtle"" hint-wrap=""true"" hint-maxLines=""3"">
+                                      Background Tasks
+                                    </text>
+                                   </subgroup>
+                                  </group>
+                                 </binding>
+                                 <binding template=""TileLarge"" hint-textStacking=""center"" branding=""name"">
+                                  <group>
+                                   <subgroup hint-weight=""1"" />
+                                   <subgroup hint-weight=""2"">
+                                    <image src=""Assets\Tile_w10_150x150.png"" hint-crop=""circle"" />
+                                   </subgroup>
+                                   <subgroup hint-weight=""1"" />
+                                  </group>
+                                  <text hint-style=""title"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
+                                    Universal Windows
+                                  </text>
+                                  <text hint-style=""caption"" hint-wrap=""true"" hint-maxLines=""3"" hint-align=""center"">
+                                    Background Task
+                                  </text>
+                                 </binding>
+                                </visual>
+                               </tile>";
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var cost = BackgroundWorkCost.CurrentBackgroundWorkCost;
+            if (cost == BackgroundWorkCostValue.High)
+            {
+                return;
+            }
+
+            var cancel = new System.Threading.CancellationTokenSource();
+            taskInstance.Canceled += (s, e) =>
+            {
+                cancel.Cancel();
+                cancel.Dispose();
+            };
+
+            var deferral = taskInstance.GetDeferral();
+            try
+            {
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.LoadXml(TileUpdaterTask.w10TileXml);
+
+                TileNotification tileNotification = new TileNotification(xmldoc);
+                TileUpdater tileUpdator = TileUpdateManager.CreateTileUpdaterForApplication();
+                tileUpdator.Update(tileNotification);
+            }
+            finally
+            {
+                deferral.Complete();
+            }
+        }
+    }
+}
--- TDC2015/TDC2015/MainPage.xaml.cs
+++ TDC2015/TDC2015/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml;
+﻿using TDC2015.BackgroundTask;
+using System;
+using System.Linq;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -15,13 +19,75 @@ namespace TDC2015
             this.InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            this.StatusLabel.Text = "Creating...";            
+            var taskRegistered = Windows.ApplicationModel.Background.BackgroundTaskRegistration.AllTasks.Select(task => task.Value.Name == nameof(TileUpdaterTask)).Count() > 0;
+            if (!taskRegistered)
+            {
+                BackgroundAccessStatus allowed = await BackgroundExecutionManager.RequestAccessAsync();
+
+                if (allowed == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity || allowed == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity)
+                {
+                    var task = new BackgroundTaskBuilder
+                    {
+                        Name = nameof(TileUpdaterTask),
+                        CancelOnConditionLoss = false,
+                        TaskEntryPoint = typeof(TileUpdaterTask).ToString(),
+                    };
+                    task.SetTrigger(new SystemTrigger(SystemTriggerType.InternetAvailable, false));
+                    task.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+                    task.Register();
+                }
+            }
+        }
+
+        private void SetTileButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.StatusLabel.Text = "Setting tile...";            
 
             TileManager.CreateTile();
 
-            this.StatusLabel.Text = "Created!";
+            this.StatusLabel.Text = "Tile has been set!";
+        }
+
+        private void RegisterTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.StatusLabel.Text = "Registering task...";
+
+            this.RegisterTask();            
+        }
+
+        private async void RegisterTask()
+        {
+            var taskRegistered = Windows.ApplicationModel.Background.BackgroundTaskRegistration.AllTasks.Select(task => task.Value.Name == nameof(TileUpdaterTask)).Count() > 0;
+            if (taskRegistered)
+            {
+                this.StatusLabel.Text = "Task already registered!";
+            }
+            else
+            {
+                BackgroundAccessStatus allowed = await BackgroundExecutionManager.RequestAccessAsync();
+
+                if (allowed == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity || allowed == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity)
+                {
+                    var task = new BackgroundTaskBuilder
+                    {
+                        Name = nameof(TileUpdaterTask),
+                        CancelOnConditionLoss = false,
+                        TaskEntryPoint = typeof(TileUpdaterTask).ToString(),
+                    };
+
+                    task.SetTrigger(new TimeTrigger(15, false));
+                    task.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+                    task.Register();                    
+
+                    this.StatusLabel.Text = "Task has been registered!";
+                }
+                else
+                {
+                    this.StatusLabel.Text = "Not allowd to register task!";
+                }                
+            }
         }
     }
 }
