bc77ccd2151adf24115eb1ca2831b78245e36200
Diego Castro <diego.castro@eldorado.org.br>
2015-07-17 13:34:01 -0300

    Added Project

---
+++ TDC2015/TDC2015/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TDC2015
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.StatusLabel.Text = "Creating...";            
+
+            TileManager.CreateTile();
+
+            this.StatusLabel.Text = "Created!";
+        }
+    }
+}
